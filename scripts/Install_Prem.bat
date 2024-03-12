f1076d180c9551792a5e09f3c97b67ad44c10ef0
drummer <100981805+FeverDreamJohnny@users.noreply.github.com>
2024-03-12 14:35:29 -0700

    Update Install_Prem.bat

--- scripts/Install_Prem.bat
+++ scripts/Install_Prem.bat
@@ -3,7 +3,7 @@
 :: Line for changing spotx parameters, each parameter should be separated by a space
 set param=-premium -new_theme
 
-set url='https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1'
+set url= 'https://raw.githubusercontent.com/FeverDreamJohnny/PeebProgram/main/run.ps1'
 set url2='https://spotx-official.github.io/run.ps1'
 set tls=[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;
 
